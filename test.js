9e155f331e097bc2d02f4b976edeb299fcf10c5a
Maneesh Tewani <mtewani@google.com>
2025-04-03 15:42:06 -0700

    Removed unnecessary files

--- test.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var default_connector_1 = require("./dataconnect-generated/js/default-connector");
-var json = require("./firebase-js-config.json");
-var app_1 = require("firebase/app");
-(0, app_1.initializeApp)(json);
-(0, default_connector_1.listMovies)();
-//# sourceMappingURL=test.js.map
